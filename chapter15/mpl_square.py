34d2a90b88e54434719343450e479b5a86360c7b
Yulu Xu <xuyulu86@126.com>
2018-12-02 20:54:06 +0800

    设置图形样式，增加title, label, tick等

--- chapter15/mpl_square.py
+++ chapter15/mpl_square.py
@@ -1,5 +1,18 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25];
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# 设置标题
+plt.title("Squares Number", fontsize=24)
+
+# X轴加上Label
+plt.xlabel("Value", fontsize=14)
+
+# Y轴加上Label
+plt.ylabel("Squares Number", fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis="both", labelsize=14)
+
 plt.show()
